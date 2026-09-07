a163fcba75c1df2a7f5bdc9acafe2968493a2df5
voiceofriyad <Contactme.riyad@yahoo.com>
2026-09-08 00:33:58 +0600

    feat: useCallback

--- Recorded Class/react/performance-optimize-intro/src/App.jsx
+++ Recorded Class/react/performance-optimize-intro/src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
-import Title from "./components/Title";
-import ShowCounter from "./components/ShowCounter";
+import { useCallback, useState } from "react";
 import Button from "./components/Button";
+import ShowCounter from "./components/ShowCounter";
+import Title from "./components/Title";
 
 function App() {
   const [counter1, setCounter1] = useState(0);
   const [counter2, setCounter2] = useState(0);
 
-  const increaseHandler1 = () => {
+  /* const increaseHandler1 = useCallback(() => {
     setCounter1(counter1 + 1);
-  };
+  }, [counter1]); */
 
-  const increaseHandler2 = () => {
+  const increaseHandler1 = useCallback(() => {
+    setCounter1((prev) => prev + 1);
+  }, []);
+
+  /* const increaseHandler2 = useCallback(() => {
     setCounter2(counter2 + 1);
-  };
+  }, [counter2]); */
+
+  const increaseHandler2 = useCallback(() => {
+    setCounter2((prev) => prev + 1);
+  }, []);
 
   return (
     <>
